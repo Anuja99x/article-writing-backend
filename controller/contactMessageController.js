31354e79c7f42d1be2b90587575430088c8b2242
Anuja-2000 <anujawij@gmail.com>
2024-01-05 23:12:39 +0530

    added crud on contactus

--- controller/contactMessageController.js
+++ controller/contactMessageController.js
@@ -1,11 +1,58 @@
 const ContactMsg = require('../model/contactMessageSchema');
 
+
+const saveMessage = (req, resp) => {
+    const messageDto = new ContactMsg({
+        messageId: req.body.messageId,
+        email: req.body.email,
+        message: req.body.message,
+        name: req.body.name,
+        replied: req.body.replied,
+        savedAt: Date.now(),
+    });
+    messageDto.save().then(result => {
+        resp.status(201).json(result);
+    }).catch(error => {
+        resp.status(500).json(error);
+    });
+}
+
+const updateMessage = (req, resp) => {
+    const messageDto = new ContactMsg({
+        messageId: req.body.messageId,
+        email: req.body.email,
+        message: req.body.message,
+        name: req.body.name,
+        replied: req.body.replied,
+        savedAt: Date.now(),
+    });
+    messageDto.updateOne().then(result => {
+        resp.status(201).json(result);
+    }).catch(error => {
+        resp.status(500).json(error);
+    });
+}
+
 const getAllMessages = (req, res) => {
     ContactMsg.find().then(result => {
         res.status(200).json(result);
-        }).catch(err => {
-            res.status(500).json(err);
-        });
-    }
+    }).catch(err => {
+        res.status(500).json(err);
+    });
+}
 
-module.exports = {getAllMessages}
+const getOneMessage = (req, res) => {
+    ContactMsg.findOne({ messageId: req.param.id }).then(result => {
+        res.status(200).json(result);
+        console.log(result);
+    }).catch(err => {
+        res.status(500).json(err);
+    });
+}
+
+module.exports = {
+    saveMessage,
+    updateMessage,
+    getAllMessages,
+    getOneMessage
+}
--- route/contactMessageRoute.js
+++ route/contactMessageRoute.js
@@ -3,6 +3,10 @@ const ContactMessageController = require('../controller/contactMessageController
 
 const router = express.Router();
 
+router.post('/add',ContactMessageController.saveMessage);
+router.patch('/update',ContactMessageController.updateMessage);
 router.get('/getAll',ContactMessageController.getAllMessages);
+router.get('/get',ContactMessageController.getOneMessage);
+
 
 module.exports = router;
