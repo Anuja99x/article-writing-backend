4936c097280b5febdb69e188d8b360115cfec6d7
Anuja-2000 <anujawij@gmail.com>
2024-02-23 21:52:54 +0530

    users count for month

--- route/userRoute.js
+++ route/userRoute.js
@@ -12,5 +12,6 @@ router.get('/count',userController.getUserCount);
 router.get('/get-writers',userController.getAllWriters);
 router.get('/get-readers',userController.getAllReaders);
 router.get('/get-user-by-name/:type/:username',userController.getUsersByUserName);
+router.get('/get-user-count-by-month/:type',userController.getUserCountByMonthAndType);
 
 module.exports = router;
