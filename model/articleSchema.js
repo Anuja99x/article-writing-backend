5dc04170009a34dde1764d743f93d953bf8779a1
upeksha-erandi99 <upekshaerandi35@gmail.com>
2024-03-12 23:47:58 +0530

    Update articleSchema.js

--- model/articleSchema.js
+++ model/articleSchema.js
@@ -32,7 +32,32 @@ const ArticleSchema = new mongoose.Schema(
     savedType: {
       type: String, // draft, completed
     },
+    coverImage: {
+      type: String,
+      default: "https://picsum.photos/500/300?random=1",
+    },
+    image1: {
+      type: String,
+      default: "https://picsum.photos/500/300?random=2",
+    },
+    image2: {
+      type: String,
+      default: "https://picsum.photos/500/300?random=3",
+    },
+    image3: {
+      type: String,
+      default: "https://picsum.photos/500/300?random=4",
+    },
+    image4: {
+      type: String,
+      default: "https://picsum.photos/500/300?random=5",
+    },
+    image5: {
+      type: String,
+      default: "https://picsum.photos/500/300?random=6",
+    },
   },
+  { timestamps: true },
   { collection: "article" }
 );
 
