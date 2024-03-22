9818084413e98074334f201a364163786b32b1d5
Anuja-2000 <anujawij@gmail.com>
2024-03-22 22:53:09 +0530

    emails are sending from the controller

--- util/sendEmail.js
+++ util/sendEmail.js
@@ -1,66 +1,67 @@
-const { SESClient,SendEmailCommand } = require( "@aws-sdk/client-ses");
+const nodemailer = require("nodemailer");
+const handlebars = require("handlebars");
+const fs = require("fs");
+const path = require("path");
 
-require('dotenv').config();
-
-const ses = new SESClient({
-    region: process.env.AWS_REGION,
-    credentials: {
-      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-    }
-  });
-
-const createSendEmailCommand = (toAddress, fromAddress) => {
-  return new SendEmailCommand({
-    Destination: {
-      /* required */
-      ToAddresses: [
-        toAddress,
-        /* more To-email addresses */
-      ],
-    },
-    Message: {
-      /* required */
-      Body: {
-        /* required */
-        Html: {
-          Charset: "UTF-8",
-          Data: "HTML_FORMAT_BODY",
-        },
-        Text: {
-          Charset: "UTF-8",
-          Data: "TEXT_FORMAT_BODY",
-        },
-      },
-      Subject: {
-        Charset: "UTF-8",
-        Data: "EMAIL_SUBJECT",
-      },
-    },
-    Source: fromAddress,
-    ReplyToAddresses: [
-      /* more items */
-    ],
-  });
-};
-
-const run = async () => {
-    console.log("Sending email");
-  const sendEmailCommand = createSendEmailCommand(
-    "anujawij@gmail.com",
-    "anujawij@gmail.com",
-  );
-
+const sendEmail = async (email, subject, payload, template,res) => {
   try {
-    const result = await ses.send(sendEmailCommand).promise();
-    console.log("Email sent", result);
-  } catch (e) {
-    console.error("Failed to send email.");
-    return e;
+    // create reusable transporter object using the default SMTP transport
+    const transporter = nodemailer.createTransport({
+      service: "yahoo",
+      secure: false,
+      host: process.env.EMAIL_HOST,
+      port: 465,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD, // naturally, replace both with your real credentials or an application-specific password
+      },
+      logger: true,
+    });
+
+    const source = fs.readFileSync(path.join(__dirname, template), "utf8");
+    const compiledTemplate = handlebars.compile(source);
+    const options = () => {
+      return {
+        from: process.env.FROM_EMAIL,
+        to: email,
+        subject: subject,
+        html: compiledTemplate(payload),
+      };
+    };
+
+    // verify connection configuration
+    /*transporter.verify(function (error, success) {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log("Server is ready to take our messages");
+      }
+    });*/
+
+
+    // Send email
+    transporter.sendMail(options(), (error, info) => {
+      if (error) {
+        console.log(error);
+      } else {
+        return res.status(200).json({
+          success: true,
+        });
+      }
+    });
+  } catch (error) {
+    console.log(error);
   }
 };
 
-module.exports = {
-    run
-}
+/*
+Example:
+sendEmail(
+  "youremail@gmail.com,
+  "Email subject",
+  { name: "Eze" },
+  "./templates/layouts/main.handlebars"
+);
+*/
 
+module.exports = sendEmail;
