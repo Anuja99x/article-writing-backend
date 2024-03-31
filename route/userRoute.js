d4da18748dd972d6999948cfcc97ab8e4472470e
shiran-dinushka <shirandinushka9@gmail.com>
2024-03-31 16:30:10 +0530

    add username updating function

--- route/userRoute.js
+++ route/userRoute.js
@@ -7,5 +7,6 @@ router.patch('/update',userController.updateUser);
 router.patch('/updatePassword',userController.updatePassword);
 router.get('/getAll',userController.getAllUsers);
 router.get('/:userId',userController.getOneUser);
+router.put('/updateName',userController.updateUserName);
 
 module.exports = router;
